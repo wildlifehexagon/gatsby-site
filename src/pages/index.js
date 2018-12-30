b4eea3f97f406dd3ab449a5476b3b8de1a78ecbc
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-30 16:18:57 -0600

    feat: add fontawesome social icons, clean up css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFreeCodeCamp,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -15,6 +23,35 @@ const IndexPage = () => (
         <hr />
         <p className="about">Web Developer</p>
         <p className="about">Chicago, IL</p>
+        <p>
+          <ul className="list-inline">
+            <li>
+              <a href="#" className="social">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social">
+                <FontAwesomeIcon icon={faFreeCodeCamp} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="social">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </li>
+          </ul>
+        </p>
       </div>
     </div>
   </Layout>
